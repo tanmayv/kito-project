5ef2a6a24c5e5f345fd841936a37e6598bf1dfd9
macpc <macpc@DESKTOP-LAPD7IO>
2022-11-12 12:40:02 +0530

    Add project files.

---
+++ kito-project.cpp
@@ -0,0 +1,49 @@
+﻿// kito-project.cpp : Defines the entry point for the application.
+//
+
+#include "kito-project.h"
+
+#define GLFW_INCLUDE_NONE
+#include <lib/include/GLFW/glfw3.h>
+
+
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+int main(void)
+{
+    GLFWwindow* window;
+    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+    GLint mvp_location, vpos_location, vcol_location;
+
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+
+    glfwMakeContextCurrent(window);
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+    return 0;
+}
